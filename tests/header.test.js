6db49ab1dbb1623219c6f094ba5d4c91c34a7893
aleksandregvrm <aleksandregvrm@gmail.com>
2024-03-16 18:07:05 +0400

    travis commit

---
+++ tests/header.test.js
@@ -0,0 +1,18 @@
+const Page = require('./helpers/page');
+
+let page;
+
+beforeEach(async () => {
+  page = await Page.build();
+  await page.goto('http://localhost:3000');
+});
+
+afterEach(async () => {
+  await page.close();
+});
+
+test('the header has the correct text', async () => {
+  const text = await page.getContentsOf('a.brand-logo');
+
+  expect(text).toEqual('Blogster');
+});
